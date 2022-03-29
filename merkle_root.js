771e001fc09acc27cccaaa3d2b462dc6b9707c78
winner <linodeBp17@gmail.com>
2022-03-29 07:07:39 -0400

    merkle complete

---
+++ merkle_root.js
@@ -0,0 +1,24 @@
+// https://medium.com/@ItsCuzzo/using-merkle-trees-for-nft-whitelists-523b58ada3f9
+//
+// 1. Import libraries. Use `npm` package manager to install
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+const wllist = require("./wllist.json");
+const oglist = require("./oglist.json");
+
+let whitelistAddresses = wllist.wllist;
+let oglistAddresses = oglist.oglist;
+const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
+
+const ogleafNodes = oglistAddresses.map(addr => keccak256(addr));
+const ogmerkleTree = new MerkleTree(ogleafNodes, keccak256, { sortPairs: true});
+
+// 4. Get root hash of the `merkleeTree` in hexadecimal format (0x)
+// Print out the Entire Merkle Tree.
+const rootHashWL = merkleTree.getHexRoot();
+const rootHashOG = ogmerkleTree.getHexRoot();
+console.log('Whitelist Merkle Tree\n', merkleTree.toString());
+console.log("WLRoot Hash: ", rootHashWL);
+console.log('OGlist Merkle Tree\n', ogmerkleTree.toString());
+console.log("OGRoot Hash: ", rootHashOG);
