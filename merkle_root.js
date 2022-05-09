5df76265b0aabf3507a294a0d6af9b01a8016703
winner <linodeBp17@gmail.com>
2022-05-09 09:43:58 -0400

    fix

--- merkle_root.js
+++ merkle_root.js
@@ -6,8 +6,9 @@ const wllist = require("./wllist.json");
 const oglist = require("./oglist.json");
 
 let whitelistAddresses = wllist.wllist;
+console.log(whitelistAddresses.length);
 let oglistAddresses = oglist.oglist;
-const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
+const leafNodes = whitelistAddresses.map(addr => keccak256(addr.wl_addresss));
 const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
 
 const ogleafNodes = oglistAddresses.map(addr => keccak256(addr));
@@ -17,7 +18,7 @@ const ogmerkleTree = new MerkleTree(ogleafNodes, keccak256, { sortPairs: true});
 // Print out the Entire Merkle Tree.
 const rootHashWL = merkleTree.getHexRoot();
 const rootHashOG = ogmerkleTree.getHexRoot();
-console.log('Whitelist Merkle Tree\n', merkleTree.toString());
+// console.log('Whitelist Merkle Tree\n', merkleTree.toString());
 console.log("WLRoot Hash: ", rootHashWL);
-console.log('OGlist Merkle Tree\n', ogmerkleTree.toString());
+// console.log('OGlist Merkle Tree\n', ogmerkleTree.toString());
 console.log("OGRoot Hash: ", rootHashOG);
